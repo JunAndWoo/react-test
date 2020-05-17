2da26ccb98ffd244396b466e87aeebc2b91fa1ca
JunAndWoo <gudwns231@naver.com>
2020-05-18 06:35:24 +0900

    Updates

---
+++ precache-manifest.f28b2744b9c9ca68a7f79ceeac834f91.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b19582ed04ffa7b4627641dd09dd86eb",
+    "url": "/react-test/index.html"
+  },
+  {
+    "revision": "5ca28b1145dc8e76c65a",
+    "url": "/react-test/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "c27c0ba1ee9ed18b1f47",
+    "url": "/react-test/static/js/2.7eccd08f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-test/static/js/2.7eccd08f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ca28b1145dc8e76c65a",
+    "url": "/react-test/static/js/main.38461814.chunk.js"
+  },
+  {
+    "revision": "bcf197a1f75092d4f61a",
+    "url": "/react-test/static/js/runtime-main.e331cf73.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-test/static/media/logo.5d5d9eef.svg"
+  }
+]);
